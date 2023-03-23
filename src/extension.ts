a75e06a67efa2774b84c4db031ab3f5c317e8ce4
auh <21078607+fanlushuai@users.noreply.github.com>
2023-03-23 17:12:27 +0800

    note-sync syncing

--- src/extension.ts
+++ src/extension.ts
@@ -7,41 +7,10 @@ export function activate(context: vscode.ExtensionContext) {
 			//这里只做一件事情 就是重新刷新配置
 			noteSync.loadConfig();
 		}),
-		//  todo 添加命令active插件 
-		//  {
-		// 	"command": "extension.enableNoteSync",
-		// 	"title": "Note Sync: Enable"
-		//   },
-		//   {
-		// 	"command": "extension.disableNoteSync",
-		// 	"title": "Note Sync: Disable"
-		//   }
-		// vscode.commands.registerCommand('extension.enableNoteSync', () => {
-		// 	//执行enable命令时 修改配置并开启插件
-		// 	console.log("loading1")
-		// }),
-
-		// vscode.commands.registerCommand('extension.disableNoteSync', () => {
-		// 	//执行disableNoteSync命令时 修改配置并关闭插件
-		// 	console.log("loading2")
-		// }),
-		//todo文件保存前需要做的事情放这里
-		// vscode.workspace.onWillSaveTextDocument((e: vscode.TextDocumentWillSaveEvent) => {
-		// 	
-		// 	if (isActive) {
-
-		// 	}
-		// }),
 
 		vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
 			noteSync.pushGitWithLongDelay();
 		}),
-		
-		// vscode.commands.registerCommand('note-sync.syncQuickly', async () => {
-		// 	vscode.commands.executeCommand('workbench.action.files.save');
-
-		// 	noteSync.pushGitWithShortDelay();
-		// }),
 		
 		vscode.commands.registerCommand('note-sync.syncQuickly', async () => {
 			vscode.commands.executeCommand('workbench.action.files.save');
